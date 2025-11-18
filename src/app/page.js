88489702a0623c666b7b5a9bd8f59bfcba0d6a30
yumkie <masanaguy@gmail.com>
2025-11-18 23:08:38 +0700

    add info

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Hero from "./Components/Hero/Hero";
+import Info from "./Components/Info/Info";
 
 export default function Home() {
   return (
     <>
     <Hero />
     <main>
-
+      <Info />
     </main>
     </>
   );
