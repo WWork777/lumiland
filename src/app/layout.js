d78d2b7399e964cd1427ea01b52128bb338b4da0
yumkie <masanaguy@gmail.com>
2025-11-20 12:55:30 +0700

    add answer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,22 @@
 import "./globals.scss";
 import Header from "./Components/Header/Header";
 import YandexMetrika from "./Components/YandexMetrika/YandexMEtrika";
-
+export const metadata = {
+  icons: {
+    icon: [
+      { rel: "icon", type: "image/svg+xml", url: "/favicon/favicon.svg" },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "96x96",
+        url: "/favicon/icon1.png",
+      },
+    ],
+    shortcut: "/favicon/favicon.ico",
+    apple: "/favicon/apple-icon.png",
+  },
+  manifest: "/favicon/site.webmanifest",
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
